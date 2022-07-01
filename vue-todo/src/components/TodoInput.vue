ca1575cca9626b1cfb253c45ba71a9cdc202d352
gwjeondev <gw4609@gmail.com>
2022-07-01 14:47:11 +0900

    [Todo App - 사용자 경험 개선] 모달 컴포넌트 등록 & [퀴즈] 모달 컴포넌트의 slot 소개 및 퀴즈 안내

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -4,14 +4,26 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="fas fa-plus addBtn"></i>
     </span>
+    <alert-model v-if="showModal" @close="showModal = false">
+      <!--
+      you can use custom content here to overwrite
+      default content
+      -->
+      <!-- <tag slot="element"></tag>는, alert-modal Component에 <slot name="element"></slot> 안의 내용과 치환된다. -->
+      <h3 slot="header">INFO</h3>
+      <span slot="body">Enter content in Todo</span>
+    </alert-model>
   </div>
 </template>
 
 <script>
+import AlertModel from './common/AlertModal.vue';
+
 export default {
   data() {
     return {
-      newTodoItem: ''
+      newTodoItem: '',
+      showModal: false
     };
   },
   methods: {
@@ -19,11 +31,16 @@ export default {
       if (this.newTodoItem) {
         this.$emit('addTodoItem', this.newTodoItem);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput() {
       this.newTodoItem = '';
     }
+  },
+  components: {
+    AlertModel
   }
 };
 </script>
---
+++ vue-todo/src/components/common/AlertModal.vue
@@ -0,0 +1,102 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <slot name="header">
+              <!-- 해당 Modal을 Component로 등록하여 사용하는 상위 Component에서 slot을 통하여 해당 content를 overwrite 한다. -->
+              <!-- default header content -->
+            </slot>
+          </div>
+          <!-- Modal Body -->
+          <div class="modal-body">
+            <slot name="body">
+              <!-- 해당 Modal을 Component로 등록하여 사용하는 상위 Component에서 slot을 통하여 해당 content를 overwrite 한다. -->
+              <!-- default body content -->
+            </slot>
+          </div>
+          <!-- Modal Footer -->
+          <div class="modal-footer">
+            <slot name="footer">
+              <!-- 해당 Modal을 Component로 등록하여 사용하는 상위 Component에서 slot을 통하여 해당 content를 overwrite 한다. -->
+              <!-- default footer content -->
+              <button class="modal-default-button" @click="$emit('close')">OK</button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
